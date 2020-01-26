d519c30834d89845557febafdbdad9a1884c2f8a
Cody Henry <codyhenry495@gmail.com>
2020-01-25 23:56:54 -0600

    Init repo

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" src>
+</head>
+<div class="container">
+    <div class="text-center pt-3">
+        <h1>Ask-a-Vegan</h1>
+    </div>
+    <div class="input input-group-text">
+        Placeholder Text
+    </div>
+</div>
+</html>
